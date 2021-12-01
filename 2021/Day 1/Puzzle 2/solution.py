aab8eefc4c7dd358daa550d655f0442d53789fe2
Sam Dyer <20486686+sam-dyer@users.noreply.github.com>
2021-12-01 19:24:30 +0000

    Add solution for day 1 puzzle 2

--- 2021/Day 1/Puzzle 2/solution.py
+++ 2021/Day 1/Puzzle 2/solution.py
@@ -1,7 +1,24 @@
-# Puzzle 1 of day 1
+# Puzzle 2 of day 1
 
 with open("puzzle_input.txt") as file:
     input_file = file.readlines()
 
+previous_sum = "NONE"
+increaced = 0
 
-        
+for i in range(0, len(input_file)):
+    try:
+        current_sum = int(input_file[i]) + int(input_file[i +1]) + int(input_file[i + 2])
+    except:
+        pass
+    if previous_sum == "NONE":
+        print(f"{current_sum} no previous sum")
+    elif previous_sum < current_sum:
+        increaced += 1
+        print(f"{current_sum} Increaced")
+    elif previous_sum > current_sum:
+        print(f"{current_sum} Decreaced") 
+
+    previous_sum = current_sum
+
+print(f"Total increaced sums: {increaced}")
